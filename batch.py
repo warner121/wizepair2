60eaa0afea7e3dff965ecea20ddb0bdba3614af3
Daniel Warner <warner121@hotmail.com>
2022-10-30 14:24:08 +0000

    run 1000 batches up to 20 heavy atoms

--- batch.py
+++ batch.py
@@ -1,19 +1,27 @@
-# importing the sys module
 import sys
 import pandas as pd
 from classes.mmp import MMP
 
 def batch(): 
+
+    # obtain in/out files from command line
     infile = sys.argv[1]
     outfile = sys.argv[2]
+
+    # read mmp data into frame and retain reference
     df = pd.read_json(infile, compression='gzip', lines=True)
     s = df.wizepair2_uuid
-    print(df.columns)
+
+    # process mmpa
     df = df.apply(lambda x: 
             MMP(x.canonical_smiles_1, x.canonical_smiles_2, strictness=5).execute(), 
             axis=1).rename('response')
+
+    # rejoin with references
     df = df.to_frame().join(s).set_index('wizepair2_uuid')
     df = df.response.explode().reset_index()
+
+    # write to out file
     df.to_json(outfile, orient='records', compression='gzip', lines=True)
     return
 
