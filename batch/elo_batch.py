b9734c1774de0cc05b0c50bc8659dd789455e639
Daniel Warner <warner121@hotmail.com>
2022-12-18 15:30:07 +0000

    scramble order of publications in bootstrapping

--- batch/elo_batch.py
+++ batch/elo_batch.py
@@ -3,22 +3,23 @@ import re
 import glob
 import logging
 import json
+import numpy as np
 import pandas as pd
 from skelo.model.elo import EloEstimator
 
 # Imports the Cloud Logging client library
-import google.cloud.logging
+#import google.cloud.logging
 
 # Instantiates a client
-client = google.cloud.logging.Client()
+#client = google.cloud.logging.Client()
 
 # Retrieves a Cloud Logging handler based on the environment
 # you're running in and integrates the handler with the
 # Python logging module. By default this captures all logs
 # at INFO level and higher
-client.setup_logging()
+#client.setup_logging()
 
-logger = client.logger("batch_task_logs")
+#logger = client.logger("batch_task_logs")
 
 def elo(df, return_ratings=False):
     
@@ -46,14 +47,23 @@ def batch():
     outfile = sys.argv[2]
 
     # read elo input data
-    logger.log_text(f'infile = {infile}, outfile = {outfile}')
-    logger.log_text(json.dumps(glob.glob(infile)))
+    #logger.log_text(f'infile = {infile}, outfile = {outfile}')
+    #logger.log_text(json.dumps(glob.glob(infile)))
     infiles = re.sub('[0-9]{12}', '*', infile)
-    logger.log_text(f'infiles = {infiles}')
-    logger.log_text(json.dumps(glob.glob(infiles)))
+    #logger.log_text(f'infiles = {infiles}')
+    #logger.log_text(json.dumps(glob.glob(infiles)))
     infiles = pd.Series(glob.glob(infiles)).sample(frac=0.5, replace=True)
-    logger.log_text(json.dumps(infiles.tolist()))
+    #logger.log_text(json.dumps(infiles.tolist()))
     df = pd.concat(infiles.apply(pd.read_csv, compression='gzip').tolist())
+    
+    # suffle publication dates
+    publication_date_shuffled = df.publication_date_greatest.unique()
+    np.random.shuffle(publication_date_shuffled)
+    df_shuffled = pd.DataFrame(
+        zip(df.publication_date_greatest.unique(), publication_date_shuffled), 
+        columns=['publication_date_greatest', 'publication_date_shuffled'])
+    df = df.merge(df_shuffled, on='publication_date_greatest').drop('publication_date_greatest', axis=1)
+    df.rename({'publication_date_shuffled': 'publication_date_greatest'}, axis=1, inplace=True)
 
     # ensure data is in chronological order
     df.publication_date_greatest = pd.to_datetime(df.publication_date_greatest)
